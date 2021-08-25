d20225370ef2d9de16572f27d03d97dc01fc6cee
paulgrammer <paulmugaya@live.com>
2021-08-25 12:25:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nn
+Created with CodeSandbox
+# vite-antd
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import tsConfigPath from "vite-tsconfig-paths";
+import styleImport from "vite-plugin-style-import";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    reactRefresh(),
+    tsConfigPath(),
+    styleImport({
+      libs: [
+        {
+          libraryName: "antd",
+          resolveStyle: (name) => {
+            return `antd/es/${name}/style/index`;
+          }
+        }
+      ]
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  }
+});
